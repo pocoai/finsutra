f2dba399a31cae0df4dcf6a834bc6d937752d29d
Aditya Vernekar <adityaditya711@gmail.com>
2023-09-23 18:40:48 +0530

    api key changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,30 +10,30 @@ export const getApi = (journey, tab) => {
   if (journey === 1) {
     switch (tab) {
       case 1:
-        return `${API}/0751cc5b-dac7-4c6e-8a95-abcf5f320c6f/generate`;
+        return `https://api.portkey.ai/v1/prompts/0751cc5b-dac7-4c6e-8a95-abcf5f320c6f/generate`;
       case 2:
-        return `${API}/3ebac38d-1acf-4ae8-b191-e73883a708a6/generate`;
+        return `https://api.portkey.ai/v1/prompts/3ebac38d-1acf-4ae8-b191-e73883a708a6/generate`;
       case 3:
-        return `${API}/f01a264a-d1a5-46c1-b5f5-1632b2bafc48/generate`;
+        return `https://api.portkey.ai/v1/prompts/f01a264a-d1a5-46c1-b5f5-1632b2bafc48/generate`;
       case 4:
-        return `${API}/fe7af321-3ec3-48a6-8e8d-04db3c5f602d/generate`;
+        return `https://api.portkey.ai/v1/prompts/fe7af321-3ec3-48a6-8e8d-04db3c5f602d/generate`;
       case 5:
-        return `${API}/a276473a-3349-4c8f-b406-f3414dd8b066/generate`;
+        return `https://api.portkey.ai/v1/prompts/a276473a-3349-4c8f-b406-f3414dd8b066/generate`;
       case 6:
-        return `${API}/5d1028eb-c771-4a96-a312-92cdbe1ccff3/generate`;
+        return `https://api.portkey.ai/v1/prompts/5d1028eb-c771-4a96-a312-92cdbe1ccff3/generate`;
       case 9:
-        return `${API}/70ab27c2-fdf2-4af4-a389-c2690f989b3f/generate`;
+        return `https://api.portkey.ai/v1/prompts/70ab27c2-fdf2-4af4-a389-c2690f989b3f/generate`;
     }
   }
   if (journey === 2) {
     switch (tab) {
       case 1:
-        return `${API}/03c51065-2924-4561-b9d3-defea9a7ab1b/generate`;
+        return `https://api.portkey.ai/v1/prompts/03c51065-2924-4561-b9d3-defea9a7ab1b/generate`;
       case 2:
-        return `${API}/5ea609ce-4321-43e3-b0f4-80b74ed2dd07/generate`;
+        return `https://api.portkey.ai/v1/prompts/5ea609ce-4321-43e3-b0f4-80b74ed2dd07/generate`;
 
       case 3:
-        return `${API}/f98542a9-03a7-4659-b84b-ab77bf09da6d/generate`;
+        return `https://api.portkey.ai/v1/prompts/f98542a9-03a7-4659-b84b-ab77bf09da6d/generate`;
     }
   }
 };
