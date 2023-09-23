f1553e26fc912cf5305155d4250a96a647411f85
Aditya Vernekar <adityaditya711@gmail.com>
2023-09-23 13:08:49 +0530

    test

--- src/app/api/query/route.js
+++ src/app/api/query/route.js
@@ -1,6 +1,6 @@
 import { connectDb } from "@/app/lib/connectDb";
 import Project from "@/models/Project";
-import { getApi } from "@/utils/api";
+import { PORTKEY, getApi } from "@/utils/api";
 import axios from "axios";
 
 import { NextResponse } from "next/server";
@@ -14,32 +14,45 @@ export async function GET(request) {
   const q = request.nextUrl.searchParams.get("q");
   const id = request.nextUrl.searchParams.get("id");
 
-  let url = `${api}?q=${q}`;
-
-  console.log(url, "url");
-
-  let result = await axios.get(`${url}`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (result.data.success) {
-    let project = await Project.findById(id);
-
-    console.log(q, "q");
-
-    project.query = q;
-    project.queryResults = result.data.content;
-
-    console.log(project, "project");
-
-    await project.save();
-    return NextResponse.json({
-      success: true,
-      data: project.queryResults,
-    });
-  } else {
-    return new Response(null, { status: 404, statusText: "Something went wrong" });
+  try {
+    let result = await axios.post(
+      api,
+      {
+        variables: {
+          idea: q,
+        },
+      },
+      {
+        headers: {
+          "x-portkey-api-key": PORTKEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(result.data, "result");
+  } catch (error) {
+    console.log(error);
   }
+
+  // if (result.data.success) {
+  //   let project = await Project.findById(id);
+
+  //   console.log(q, "q");
+
+  //   project.query = q;
+  //   project.queryResults = result.data.content;
+
+  //   console.log(project, "project");
+
+  //   await project.save();
+  //   return NextResponse.json({
+  //     success: true,
+  //     data: project.queryResults,
+  //   });
+  // } else {
+  //   return new Response(null, { status: 404, statusText: "Something went wrong" });
+  // }
+  return NextResponse.json({
+    success: true,
+  });
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,13 @@
 export const API = process.env.BACKEND_API;
+export const PORTKEY = process.env.PORTKEY;
 
 export const getApi = (journey, tab) => {
   if (journey === 1) {
     switch (tab) {
       case 1:
-        return `${API}/j1/tab1`;
+        return `${API}/0751cc5b-dac7-4c6e-8a95-abcf5f320c6f/generate`;
       case 2:
-        return `${API}/j1/tab2`;
+        return `${API}/3ebac38d-1acf-4ae8-b191-e73883a708a6/generate`;
     }
   }
 };
