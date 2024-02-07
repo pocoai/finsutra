c461799b604629943ce21b38da7bc335fa656fb8
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2024-02-08 00:41:13 +0530

    demo video added

--- src/app/demo/page.jsx
+++ src/app/demo/page.jsx
@@ -8,7 +8,7 @@ const Demo = () => {
         <div>
             <h1 className="text-2xl font-bold">Overview of Finsutra</h1>
             <div className='w-full h-screen flex justify-center items-center'>
-                <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+                <ReactPlayer url='https://www.loom.com/share/1ce077d6286348cc8cd549a484bc326e?sid=0cac6257-f12e-4ef3-b4bf-4cfc7e52e757' />
             </div>
         </div>
      );
