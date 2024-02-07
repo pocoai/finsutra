f57dadfb3b0b2055dd5f964a367335dc5edcc870
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2024-02-08 00:53:56 +0530

    add loom video

--- src/app/demo/page.jsx
+++ src/app/demo/page.jsx
@@ -8,7 +8,9 @@ const Demo = () => {
         <div>
             <h1 className="text-2xl font-bold">Overview of Finsutra</h1>
             <div className='w-full h-screen flex justify-center items-center'>
-                <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/1ce077d6286348cc8cd549a484bc326e?sid=40588d91-b993-4932-a1b4-f1cca1983318" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+                <div className='bg-red-300 min-w-[500px] min-h-[500px]'>
+                    <iframe className='w-full h-[500px]' src="https://www.loom.com/embed/1ce077d6286348cc8cd549a484bc326e?sid=40588d91-b993-4932-a1b4-f1cca1983318" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
             </div>
         </div>
      );
